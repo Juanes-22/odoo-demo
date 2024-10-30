27bb25ea4101caffd1834caa70e31f80b4112201
Juan Esteban García Martínez <juan.garcia@premexcorp.com>
2024-10-30 07:19:01 -0500

    feat(modules): add salesperson and buyer to estate property model
    add salesperson and buyer to estate property model

--- modules/estate/models/models.py
+++ modules/estate/models/models.py
@@ -43,6 +43,11 @@ class EstateProperty(models.Model):
         default="new",
     )
     property_type_id = fields.Many2one("estate.property.type", string="Property Type")
+    buyer_id = fields.Many2one("res.partner", string="Buyer", copy=False)
+    salesperson_id = fields.Many2one(
+        "res.users", string="Salesperson", default=lambda self: self.env.user
+    )
+
 
 class EstatePropertyType(models.Model):
     _name = "estate.property.type"
